a7cd0497d77815331ed8ee7765b28db8998e9e02
Andre Frank <andrefrank.mail@t-online.de>
2018-11-29 22:33:31 +0100

    Refactored expandable section code ( should be easier to adapt to realm database episodes

--- SerienTracker/Controller/EpisodesListVC.swift
+++ SerienTracker/Controller/EpisodesListVC.swift
@@ -8,10 +8,19 @@
 
 import UIKit
 
-// Used to expand/collapse episodes in season sections'
-struct ShowHideEpsiodes {
+// Used to expand/collapse structure to show hide sections (Seasons and contained episodes)
+struct ShowHideSection {
     var isExpanded: Bool
-    var episodes: [ShowEpisodenInformation]
+    //Removed 29.11.2018
+    //var episodes: [ShowEpisodenInformation]
+    
+    //Added 29.11.2018
+    //One section contains an section number and assigned episodes
+    var episodesBySection=[ShowEpisodenInformation]()
+    //Added 29.11.2018
+    mutating func expandSection(expand:Bool){
+        isExpanded=expand
+    }
 }
 
 class EpisodesListVC: UITableViewController, UIViewControllerPreviewingDelegate {
@@ -20,9 +29,14 @@ class EpisodesListVC: UITableViewController, UIViewControllerPreviewingDelegate 
     
     var showepList = ShowEpListApi()
     
-    // Holds episodes grouped by seasons
+    
     // >> var groupedEpisodes = [[ShowEpisodenInformation]]()
-    var expandableGroupedEpisodes = [[ShowHideEpsiodes]]()
+    
+    //Removed 29.11.2018
+   //var expandableGroupedEpisodes = [[ShowHideSection]]()
+    
+    //Added 29.11.2018
+    var expandableSections=[ShowHideSection]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,7 +53,7 @@ class EpisodesListVC: UITableViewController, UIViewControllerPreviewingDelegate 
         }
         
         // Read all episodes from show
-        showepList.getEpList(id: showInfo!) { [weak self] episoden in
+        showepList.getEpList(id: showInfo!) { [unowned self] episoden in
             if let episoden = episoden {
                 // Create dictionary for each season
                 let groupedDictionary = Dictionary(grouping: episoden) { (episode) -> Int in
@@ -48,16 +62,21 @@ class EpisodesListVC: UITableViewController, UIViewControllerPreviewingDelegate 
                 
                 // Get keys in sorted order (1,2,3...)
                 let keys = groupedDictionary.keys.sorted()
-                
+               
                 // Fill array for each key
                 // groupedEpisodes[season n][episode n]
                 keys.forEach({ key in
+
+                 //Removed 29.11.2018
                     
-                    self?.expandableGroupedEpisodes.append([ShowHideEpsiodes(isExpanded: true, episodes: groupedDictionary[key]!)])
+                 //   self?.expandableGroupedEpisodes.append([ShowHideEpsiodes(isExpanded: true, episodes: groupedDictionary[key]!)])
+                    
+                 //Added 29.11.2018
+                    self.expandableSections.append(ShowHideSection(isExpanded: true, episodesBySection:groupedDictionary[key]!))
                 })
                 
                 // Refresh tableview
-                self?.tableView.reloadData()
+                self.tableView.reloadData()
             }
         }
     }
@@ -65,23 +84,36 @@ class EpisodesListVC: UITableViewController, UIViewControllerPreviewingDelegate 
     // MARK: - Table view data source
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return expandableGroupedEpisodes.count
+        
+        //Added 29.11.2018
+         return expandableSections.count
+        
+        //Removed 29.11.2018
+        //return expandableGroupedEpisodes.count
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // number of episodes in seasons (section points to actual season)
         
-        let expandedRows = expandableGroupedEpisodes[section][0].isExpanded ? expandableGroupedEpisodes[section][0].episodes.count : 0
+        //Added 29.11.2018
+        let expandableRows=expandableSections[section].isExpanded ? expandableSections[section].episodesBySection.count : 0
         
-        return expandedRows
+        
+        //Removed 29.11.2018
+        //let expandableRows = expandableGroupedEpisodes[section][0].isExpanded ? expandableGroupedEpisodes[section][0].episodes.count : 0
+        
+        return expandableRows
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "episodenCell") as! epCell
         
+        // Rremoved 29.11.2018
         // Get episode from season(section) and row
-        let episode = expandableGroupedEpisodes[indexPath.section][0].episodes[indexPath.row]
+        //let episode = expandableGroupedEpisodes[indexPath.section][0].episodes[indexPath.row]
         
+        //Added 29.11.2018
+        let episode = expandableSections[indexPath.section].episodesBySection[indexPath.row]
        
         cell.setEp(episode: episode)
         
@@ -102,7 +134,14 @@ class EpisodesListVC: UITableViewController, UIViewControllerPreviewingDelegate 
         //>> Todo read value from realm database
         
         //Invert seen flag when selected
-        expandableGroupedEpisodes[indexPath.section][0].episodes[indexPath.row].seen = !(expandableGroupedEpisodes[indexPath.section][0].episodes[indexPath.row].seen ?? false)
+        //Removed 29.11.2018
+//        expandableGroupedEpisodes[indexPath.section][0].episodes[indexPath.row].seen = !(expandableGroupedEpisodes[indexPath.section][0].episodes[indexPath.row].seen ?? false)
+        
+        //Added 29.11.2018
+        //Invert seen flag by selecting the cell
+        expandableSections[indexPath.section].episodesBySection[indexPath.row].seen = !(expandableSections[indexPath.section].episodesBySection[indexPath.row].seen ?? false)
+        
+        
 //
         // Refresh modified rows(episodes) from tableview
         tableView.reloadRows(at: [indexPath], with: UITableView.RowAnimation.none)
@@ -118,7 +157,10 @@ class EpisodesListVC: UITableViewController, UIViewControllerPreviewingDelegate 
         // Create a detail view controller and set its properties.
         guard let detailViewController = storyboard?.instantiateViewController(withIdentifier: "preview") as? previewViewController else { return nil }
         
-        let previewDetail = expandableGroupedEpisodes[indexPath.section][0].episodes[indexPath.row]
+        //Removed 29.11.2018
+        //let previewDetail = expandableGroupedEpisodes[indexPath.section][0].episodes[indexPath.row]
+        let previewDetail = expandableSections[indexPath.section].episodesBySection[indexPath.row]
+        
         detailViewController.episodenDetail = previewDetail
         
         previewingContext.sourceRect = cell.frame
@@ -147,7 +189,12 @@ extension EpisodesListVC {
       
         // Use the globally used text style to show the Season sections
         // >> let title = "Season \(groupedEpisodes[section][0].season!)"
-        let title = "Season \(expandableGroupedEpisodes[section][0].episodes[0].season!)"
+        
+        //Removed 29.11.2018
+        //let title = "Season \(expandableGroupedEpisodes[section][0].episodes[0].season!)"
+        
+        //Added 29.11.2018
+        let title = "Season \(expandableSections[section].episodesBySection[0].season!)"
         
         let strokeTextAttributes = [
             NSAttributedString.Key.foregroundColor: UIColor.white,
@@ -173,15 +220,26 @@ extension EpisodesListVC {
         
         // get all episodes from season
         var indexPaths = [IndexPath]()
-        for row in expandableGroupedEpisodes[expandSection][0].episodes.indices {
+        
+        //Removed 29.11.2018
+        //        for row in expandableGroupedEpisodes[expandSection][0].episodes.indices {
+//            let index = IndexPath(row: row, section: expandSection)
+//            indexPaths.append(index)
+//        }
+        //Added 29.11.2018
+        for row in expandableSections[expandSection].episodesBySection.indices {
             let index = IndexPath(row: row, section: expandSection)
             indexPaths.append(index)
         }
+        //Prepare expandable sections
+        let isExpanded = expandableSections[expandSection].isExpanded
+        expandableSections[expandSection].expandSection(expand: !isExpanded)
         
-        // Get expand flag
-        let isExpanded = expandableGroupedEpisodes[expandSection][0].isExpanded
-        // Revert it
-        expandableGroupedEpisodes[expandSection][0].isExpanded = !isExpanded
+        //Removed 29.11.2018
+//        // Get expand flag
+//        let isExpanded = expandableGroupedEpisodes[expandSection][0].isExpanded
+//        // Revert it
+//        expandableGroupedEpisodes[expandSection][0].isExpanded = !isExpanded
         
         if isExpanded { // Yes, fold
             tableView.deleteRows(at: indexPaths, with: UITableView.RowAnimation.fade)
